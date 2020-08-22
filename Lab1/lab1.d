7d992c5e0367516d9edb595002c5e03696dbea87
jiahzhang <jhzhang2@outlook.com>
2020-08-22 13:51:16 -0500

    Add files via upload
    ASCII to fixed-point on LCD

---
+++ Lab1/lab1.d
@@ -0,0 +1,6 @@
+.\lab1.o: Lab1.c
+.\lab1.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\lab1.o: ../inc/ST7735.h
+.\lab1.o: ../inc/PLL.h
+.\lab1.o: fixed.h
+.\lab1.o: ../inc/tm4c123gh6pm.h
---
+++ Lab1/st7735_portd.d
@@ -0,0 +1,6 @@
+.\st7735_portd.o: ..\inc\ST7735_PortD.c
+.\st7735_portd.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\st7735_portd.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\st7735_portd.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\st7735_portd.o: ..\inc\ST7735_PortD.h
+.\st7735_portd.o: ..\inc\../inc/tm4c123gh6pm.h
